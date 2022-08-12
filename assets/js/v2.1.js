6e372b00cdc6864eb70273c0cf8772559868cc08
JacielJM <jacimenera@gmail.com>
2022-08-11 23:28:28 -0500

    desarrollo v2

---
+++ assets/js/v2.1.js
@@ -0,0 +1,54 @@
+const pokeCards = [];
+const poke = document.getElementById('busqueda');
+console.log(poke.value);
+const button = document.getElementById('button-addon2');
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	fetchPokemon(poke.value);
+});
+
+class PokeCard {
+	constructor(data) {
+		this.nombre = data.name;
+		this.img = data.sprites.front_default;
+		this.id = data.id;
+		this.borrar = false;
+	}
+}
+
+function fetchPokemon(poke) {
+	fetch(`https://pokeapi.co/api/v2/pokemon/${poke.toLowerCase()}`)
+		.then((res) => res.json())
+		.then((data) => pokeCards.push(new PokeCard(data)));
+	displayPokemon();
+}
+
+// function buildCards(pokecards) {
+// 	let arr = pokecards.map((pokecard) => buildCard(pokecard));
+// 	return arr;
+// }
+
+function buildCard(pokecard) {
+	let divCol = document.createElement('div');
+	divCol.setAttribute('class', 'col');
+
+	divCol.innerHTML += `<div class="card h-100">
+                <button type="button" class="btn-close" aria-label="Close"></button>
+                <img src="${pokecard.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${pokecard.nombre.toUpperCase()}</h5>
+                    <p class="card-text">Id: ${pokecard.id}</p>
+                </div>
+            </div>`;
+	return divCol;
+}
+function displayPokemon() {
+	let pokeContainer = document.querySelector('#gallery');
+	pokeContainer.innerHTML = '';
+	for (let i = 0; i < pokeCards.length; i++) {
+		let result = buildCard(pokeCards[i]);
+		pokeContainer.appendChild(result);
+	}
+	document.querySelector('#busqueda').value = '';
+}
+fetchPokemon();
---
+++ assets/js/v2.js
@@ -0,0 +1,71 @@
+const pokeCards = [];
+const poke = document.getElementById('busqueda');
+console.log(poke.value);
+const button = document.getElementById('button-addon2');
+poke.addEventListener('click', addPokecard(poke.value));
+
+class PokeCard {
+	constructor(nombre, img, id) {
+		this.nombre = nombre;
+		this.img = img;
+		this.id = id;
+		this.borrar = false;
+	}
+
+	borrar() {
+		this.borrar = true;
+	}
+}
+
+function buildCard(pokecard, index) {
+	let divCol = document.createElement('div');
+	divCol.setAttribute('class', 'col');
+	divCol.setAttribute('id', index);
+
+	divCol.innerHTML += `<div class="card h-100">
+                <button id="b${index}" type="button" class="btn-close" aria-label="Close"></button>
+                <img src="${pokecard.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${pokecard.name.toUpperCase()}</h5>
+                    <p class="card-text">Id: ${pokecard.id}</p>
+                </div>
+            </div>`;
+	pokecard.borrar && deleteCard(index);
+	let equis = document.getElementById(`b${index}`);
+	equis.addEventListener('click', borrar);
+	return divCol;
+}
+
+function buildCards(pokecards) {
+	let arr = pokecards.map((pokecard, index) => buildCard(pokecard, index));
+	return arr;
+}
+
+function displayPokeCards() {
+	let pokeContainer = document.querySelector('#gallery');
+	document.querySelector('#busqueda').value = '';
+	let result = buildCards(pokeCards);
+	for (let i = 0; i < result.length; i++) {
+		pokeContainer.appendChild(result[i]);
+	}
+}
+
+function addPokecard(poke) {
+	fetch(`https://pokeapi.co/api/v2/pokemon/${poke.toLowerCase()}`)
+		.then((res) => res.json())
+		.then((data) => {
+			nuevo = new PokeCard(data.name, data.sprites.front_default, data.id);
+			pokeCards.push(nuevo);
+		});
+	console.log(pokeCards);
+	document.getElementById('busqueda').value = '';
+	displayPokeCards();
+}
+
+function borrar(event) {
+	const index = event.target.id;
+	pokeCards.splice(index, 1);
+	displayPokeCards();
+}
+
+displayPokeCards();
