4a1aee69e3670a9b039fa3af8d344365eb724f2e
JacielJM <jacimenera@gmail.com>
2022-08-13 23:15:09 -0500

    colores random y alerta cuando no se indica el pokemon

--- assets/js/v1.js
+++ assets/js/v1.js
@@ -1,11 +1,39 @@
+// Colores random background
+const getRandomNumber = maxNum => {
+	return Math.floor(Math.random() * maxNum);
+};
+const getRandomColor = () => {
+	const h = getRandomNumber(360);
+	const s = getRandomNumber(100);
+	const l = getRandomNumber(100);
+
+	return `hsl(${h}deg, ${s}%, ${l}%)`;
+};
+const setBackgroundColor = () => {
+	const randomColor = getRandomColor();
+	document.getElementById('app').style.backgroundColor = randomColor;
+};
+
+// Pokémons
+
 const pokemon = document.getElementById('busqueda');
 const button = document.getElementById('button-addon2');
 const gallery = document.getElementById('gallery');
 const reset = document.getElementById('reset');
+const alerta = document.getElementById('alert');
+const randomColor = document.getElementById('randomColors');
 let cards = [];
+let crazy = false;
+
+randomColor.addEventListener('change', e => {
+	if (e.target.checked) crazy = true;
+});
 
 button.addEventListener('click', e => {
 	e.preventDefault();
+	if (crazy) {
+		setBackgroundColor();
+	}
 	fetchPokemon(pokemon.value);
 });
 
@@ -15,10 +43,13 @@ pokemon.addEventListener('keypress', e => {
 
 reset.addEventListener('click', clearPokemon);
 
-function fetchPokemon(poke = undefined) {
-	fetch(`https://pokeapi.co/api/v2/pokemon/${poke.toLowerCase()}`)
-		.then(res => res.json())
-		.then(data => addPokemon(data));
+function fetchPokemon(poke = '') {
+	if (poke) {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${poke.toLowerCase()}`)
+			.then(res => res.json())
+			.then(data => addPokemon(data));
+	}
+	alerta.innerHTML = 'Por favor ingresa un nombre o ID';
 }
 
 function addPokemon(pokemon) {
@@ -31,7 +62,7 @@ function addPokemon(pokemon) {
 
 	const closeBtn = document.createElement('button');
 	closeBtn.setAttribute('type', 'button');
-	closeBtn.setAttribute('class', 'btn-close');
+	closeBtn.setAttribute('class', 'btn-close p-3');
 	closeBtn.setAttribute('ariaLabel', 'Close');
 
 	const imgPokemon = document.createElement('img');
@@ -65,6 +96,7 @@ function saveCard(card) {
 }
 
 function displayCards(cards = []) {
+	alerta.innerHTML = 'Atrápalos a todos!';
 	if (!cards.length) gallery.innerHTML = '';
 	if (cards.length) {
 		gallery.innerHTML = '';
@@ -89,6 +121,7 @@ function deleteCard(e) {
 function clearPokemon() {
 	gallery.innerHTML = '';
 	cards = [];
+	document.getElementById('app').style.backgroundColor = 'rgb(233, 247, 244';
 	displayCards(cards);
 }
 
